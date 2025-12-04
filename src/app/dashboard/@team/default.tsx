55c9d677d5251505e61faf995dc99bea394d67ec
dengchan <h-dengchan@voyah.com.cn>
2025-12-04 16:41:33 +0800

    feat: 添加仪表盘并行路由和默认子路由的默认组件

---
+++ src/app/dashboard/@team/default.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TeamDefault() {
+  return (
+    <div>
+      <h2 className='text-xl font-semibold text-blue-600'>Team - Default</h2>
+      <p className='text-sm text-gray-500'>这是其他路由的默认 team 内容</p>
+    </div>
+  )
+}
