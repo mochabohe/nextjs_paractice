74df6904985dc8cdcdd3c4688f69d686843273ba
dengchan <h-dengchan@voyah.com.cn>
2025-12-04 12:32:00 +0800

    feat:修改样式

--- src/app/(dengchan)/page.tsx
+++ src/app/(dengchan)/page.tsx
@@ -5,7 +5,7 @@ function page() {
   return (
     <div className='absolute inset-0 pt-20'>
       <Image src='/next.svg' width={200} height={200} alt='Logo' />
-      <div className='fs-3xl text-center'>chengluoning程落柠的首页</div>
+      <div className='text-3xl text-center'>chengluoning程落柠的首页</div>
     </div>
   )
 }
--- src/app/(dengchan)/scale/page.tsx
+++ src/app/(dengchan)/scale/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function page() {
-  return <div className='fs-3xl text-center'>scale程落柠</div>
+  return <div className='text-3xl text-center'>scale程落柠</div>
 }
