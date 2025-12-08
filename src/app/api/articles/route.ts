94b2aa07a4de71bc5d0a02eedf97a4f7a6001bee
dengchan <h-dengchan@voyah.com.cn>
2025-12-08 15:10:09 +0800

    feat: 添加文章API接口，支持分页查询、搜索和新增文章

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -1,7 +1,39 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import db from '@/db'
 
-export async function GET(request: Request) {}
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const pagenum = Number(searchParams.get('pagenum')) || 1
+  const pagesize = Number(searchParams.get('pagesize')) || 10
+  const query = searchParams.get('query') || ''
+
+  const data = db.data.posts
+
+  let filteredData = data
+  if (query) {
+    filteredData = data.filter((item) => {
+      const { id, ...rest } = item
+      return Object.values(rest).some((value) =>
+        String(value).toLowerCase().includes(query.toLowerCase())
+      )
+    })
+  }
+
+  const total = filteredData.length
+  const startIndex = (pagenum - 1) * pagesize
+  const endIndex = startIndex + pagesize
+  filteredData =
+    startIndex >= endIndex ? [] : filteredData.slice(startIndex, endIndex)
+
+  return NextResponse.json({
+    code: 0,
+    message: '获取成功',
+    data: {
+      list: filteredData,
+      total,
+    },
+  })
+}
 
 export async function POST(request: Request) {
   const data = await request.json()
